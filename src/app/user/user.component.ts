4dd91fdd5c6a2a8648b81cfeabad505ecdbe50f3
HaiPham <phamphamhai99@gmail.com>
2024-10-17 01:08:06 +0700

    Init todo application

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  Output,
+} from '@angular/core';
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectedUser() {
+    this.select.emit(this.user.id);
+  }
+}
