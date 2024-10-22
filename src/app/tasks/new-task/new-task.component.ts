72fe9473076a20da18e688c705410feb01d75dcb
HaiPham <phamphamhai99@gmail.com>
2024-10-23 01:04:25 +0700

    Handle-create-new-task-feature

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,30 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   // void mean there is no value to be returned from output function
   @Output() cancel = new EventEmitter<void>();
+  @Output() submit = new EventEmitter<any>();
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
+  onSubmit() {
+    const task = {
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    };
+    this.submit.emit(task);
+  }
 
   onCancle() {
     this.cancel.emit();
